004b39bf47d47047a0efe7211c9553a95b891b9f
Podwig <90828766+Podwig@users.noreply.github.com>
2021-09-22 20:45:37 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paketversand
+{
+    class Program
+    {
+              static void Main(string[] args)
+        { 
+            //------------------Aufgabe1----------------------------
+            //Notification
+            Console.WriteLine("Herzlich Wilkommen");
+            Console.WriteLine("Dieses Programm berechnet die benötigten Briefmarkenwert");
+
+            //------------------Aufgabe2----------------------------
+            //Deklaration
+            Int32 iLaenge;
+            Int32 iBreite;
+            Int32 iMaterialstaerke;
+
+            Double dDichte;
+            Double dMasse;
+
+            //Defenition
+            iMaterialstaerke = 3; //cm
+            dDichte = 0.0007; //cm^3
+
+            //Notification
+            Console.Write("Bitte geben Sie die Laenge in cm ein:");
+            iLaenge = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Bitte geben Sie die Breite in cm ein:");
+            iBreite = Convert.ToInt32(Console.ReadLine());
+
+            //------------------Aufgabe3---------------------------
+            Console.Write("Die Masze sind(Ixbxh):");
+            Console.WriteLine(iLaenge + "x" + iBreite + "x" + iMaterialstaerke);
+
+            //------------------Aufgabe4-------------------------
+            dMasse = iBreite * iLaenge * iMaterialstaerke * dDichte; //kg
+            Console.WriteLine("Die Masse betraegt:" + dMasse + "kg");
+
+            //Don´t Close
+            Console.ReadLine();
+        }
+    }
+}
